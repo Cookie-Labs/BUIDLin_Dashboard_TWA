1bce720cfc239249d6f9cfc008b5ee1e44e08f59
kdy <kkykdy4334@gmail.com>
2023-12-06 23:03:11 +0900

    test: test console

--- app/(dashboard)/created-event/page.tsx
+++ app/(dashboard)/created-event/page.tsx
@@ -1,16 +1,26 @@
-// import { myEventsData } from '@/states/formUserState';
-// import { useRecoilValue } from 'recoil';
+'use client';
+
+import { useState, useEffect } from 'react';
+import { myEventsData } from '@/states/formUserState';
+import { useRecoilValue } from 'recoil';
 
 import ScrollToTopButton from '@/components/scroll-to-top-button';
 
 export default function CreatedEventPage() {
-  // const eventsData = useRecoilValue(myEventsData);
+  const eventsData = useRecoilValue(myEventsData);
+  const [currentHref, setCurrentHref] = useState('');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setCurrentHref(window.location.href);
+    }
+  }, []);
 
   return (
     <div className="relative flex flex-col items-center justify-center gap-[3rem]">
       <div>CreatedEventPage</div>
-      {/* <div>{eventsData}</div> */}
-      {/* <div>{window.location.href}</div> */}
+      <div>{JSON.stringify(eventsData, null, 2)}</div>
+      <div>{currentHref}</div>
       <ScrollToTopButton />
     </div>
   );
