a661a775e60f7f7c9ff1bcbe9130e1176229cb1f
kdy <kkykdy4334@gmail.com>
2023-12-06 22:37:48 +0900

    build: build error fix

--- app/(dashboard)/created-event/page.tsx
+++ app/(dashboard)/created-event/page.tsx
@@ -1,16 +1,16 @@
-import { myEventsData } from '@/states/formUserState';
-import { useRecoilValue } from 'recoil';
+// import { myEventsData } from '@/states/formUserState';
+// import { useRecoilValue } from 'recoil';
 
 import ScrollToTopButton from '@/components/scroll-to-top-button';
 
 export default function CreatedEventPage() {
-  const eventsData = useRecoilValue(myEventsData);
+  // const eventsData = useRecoilValue(myEventsData);
 
   return (
     <div className="relative flex flex-col items-center justify-center gap-[3rem]">
       <div>CreatedEventPage</div>
-      <div>{JSON.stringify(eventsData)}</div>
-      <div>{window.location.href}</div>
+      {/* <div>{eventsData}</div> */}
+      {/* <div>{window.location.href}</div> */}
       <ScrollToTopButton />
     </div>
   );
