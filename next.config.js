809f91d307e439b24238b2bd7b249a59d7a4311d
kdy <kkykdy4334@gmail.com>
2023-12-07 20:46:59 +0900

    fix: change next.config.js images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-// module.exports = async (phase, { defaultConfig }) => {
-    /**
-     * @type {import('next').NextConfig}
-     */
-    const nextConfig = {
-      reactStrictMode: true, // react <React.StrictMode>
-      swcMinify: true, // like Terser
-    };
-    return nextConfig;
-// };
+module.exports = {
+  reactStrictMode: true, // react <React.StrictMode>
+  swcMinify: true, // like Terser
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'buidlin-images.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
